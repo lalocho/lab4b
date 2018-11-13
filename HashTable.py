625d349074c23664e8c232ef96e85e2d6a9bceb9
luis ochoa <lalocho13@gmail.com>
2018-11-12 22:14:52 -0800

    Add files via upload

---
+++ HashTable.py
@@ -0,0 +1,103 @@
+# Luis Ochoa
+# 80508534
+# 11:30
+
+
+class Node(object):
+    password = ""
+    next = None
+
+    def __init__(self, password, next):
+        self.password = password
+        self.next = next
+
+# HashTable class using chaining.
+
+
+class ChainingHashTable:
+    # Constructor with optional initial capacity parameter.
+    # Assigns all buckets with an empty list.
+    # All insert and search methods are the same with exceot the actual hash function used
+    def __init__(self, initial_capacity):
+        # initialize the hash table with empty bucket list entries.
+        self.table = [None]*initial_capacity
+        self.collision_counter = 0  # used to calculate average collision in table
+        self.input_counter = 0      # used to calculate load factor of table
+
+    # Inserts a new item into the hashtable.
+    # checks if item already exists in table if it does, it doesnt do anything
+    # inserts into beginning of linked list for chaining
+    def insert_a(self, item):
+        # get the bucket list where this item will go.
+        bucket = hash(item) % len(self.table)
+        # inserts Node into the beginning setting whatever was already inside as .next
+        temp_node = self.table[bucket]
+        if temp_node is not None:
+            self.collision_counter += 1
+        while temp_node is not None:
+            if temp_node.password == item:
+                return
+        self.input_counter += 1
+        self.table[bucket] = Node(item, self.table[bucket])
+        return
+
+    def insert_b(self, item):
+        # get the bucket list where this item will go.
+        bucket = hash(item) % len(self.table)
+        # inserts Node into the beginning setting whatever was already inside as .next
+        temp_node = self.table[bucket]
+        while temp_node is not None:
+            if temp_node.password == item:
+                return
+        self.table[bucket] = Node(item, self.table[bucket])
+        return
+
+    def insert_c(self, item):
+        # get the bucket list where this item will go.
+        bucket = hash(item) % len(self.table)
+        # inserts Node into the beginning setting whatever was already inside as .next
+        temp_node = self.table[bucket]
+        while temp_node is not None:
+            if temp_node.password == item:
+                return
+        self.table[bucket] = Node(item, self.table[bucket])
+        return
+    # Searches for an item with matching key in the hash table.
+    # Returns the item if found, or None if not found.
+
+    def search_a(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        temp_node = self.table[bucket]
+        while temp_node is not None:
+            if temp_node.password == key:
+                return True
+            temp_node = temp_node.next
+        return False
+
+    def search_b(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        temp_node = self.table[bucket]
+        while temp_node is not None:
+            if temp_node.password == key:
+                return True
+            temp_node = temp_node.next
+        return False
+
+    def search_c(self, key):
+        # get the bucket list where this key would be.
+        bucket = hash(key) % len(self.table)
+        temp_node = self.table[bucket]
+        while temp_node is not None:
+            if temp_node.password == key:
+                return True
+            temp_node = temp_node.next
+        return False
+
+
+
+
+
+
+
---
+++ lab4b.py
@@ -0,0 +1,102 @@
+# Luis Ochoa
+# 80508534
+# 11:30
+from HashTable import ChainingHashTable, Node
+
+
+def solution_a():
+    f = open('test.txt')
+    line = f.readline()
+    # Size is set so the load factor is .75
+    dict = ChainingHashTable(473312)
+    while line:
+        # getting only the passwords from each line
+        try:  # ignores line if irregularity with data
+            dict.insert_a((line.rstrip().lower()))
+        except:
+            print()
+        line = f.readline()
+
+    f.close()
+    word_input = input("word you want to check anagrams for")
+    anagram_printer(word_input, dict)
+    print("load factor is ",load_factor(dict))
+    print("average collision is ", average_collision(dict))
+def solution_b():
+    f = open('test.txt')
+    line = f.readline()
+    # Size is set so the load factor is .75
+    dict = ChainingHashTable(473312)
+    while line:
+        # getting only the passwords from each line
+        try:  # ignores line if irregularity with data
+            dict.insert_a((line.rstrip().lower()))
+        except:
+            print()
+        line = f.readline()
+
+    f.close()
+    word_input = input("word you want to check anagrams for")
+    anagram_printer(word_input, dict)
+    print("load factor is ", load_factor(dict))
+    print("average collision is ", average_collision(dict))
+
+def solution_c():
+    f = open('test.txt')
+    line = f.readline()
+    # Size is set so the load factor is .75
+    dict = ChainingHashTable(473312)
+    while line:
+        # getting only the passwords from each line
+        try:  # ignores line if irregularity with data
+            dict.insert_a((line.rstrip().lower()))
+        except:
+            print()
+        line = f.readline()
+
+    f.close()
+    word_input = input("word you want to check anagrams for")
+    anagram_printer(word_input, dict)
+    print("load factor is ", load_factor(dict))
+    print("average collision is ", average_collision(dict)
+
+
+# used to calculate average
+# uses counter included in hashtable class
+def average_collision(dict):
+    length = len(dict.table)
+
+    return dict.collision_counter/length
+
+# used to calculate load factor
+# uses counter included in hashtable class
+def load_factor(dict):
+
+    length = len(dict.table)
+    return dict.input_counter / length
+
+
+# prints all anagrams of a word that are part of the dictionary
+def anagram_printer(key,dict):
+
+    anagrams = all_perms(key)
+    for i in range(1, len(anagrams)):
+        if dict.search_a(anagrams[i]):
+            print(anagrams[i])
+
+# computes all permutations of a word and returns a list/array containing them
+def all_perms(elements):
+    temp_word = ""
+    if len(elements) <=1:
+        return elements
+    else:
+        tmp = []
+        for perm in all_perms(elements[1:]):
+            for i in range(len(elements)):
+                tmp.append(perm[:i] + elements[0:1] + perm[i:])
+        return tmp
+
+
+def main():
+    solution_a()
+main()
